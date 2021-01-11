d3386840d4af90ca1ea04b43051c0722aa2832f5
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-10 19:47:25 -0600

    Added table content to the table component folder and imported this info to the app.js file.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import React from 'react';
 import Header from './components/Header/index';
+import Table from './components/Table/index';
 
 
 function App() {
   return (
     <div>
       <Header>
-
       </Header>
-      
+      <Table/>
     </div>
   );
 }
