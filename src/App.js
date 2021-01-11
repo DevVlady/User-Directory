187c0eca9d7a123098b3060903f55963541500e8
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-10 19:26:04 -0600

    Added the table content inside the table components folder.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Header>
 
       </Header>
+      
     </div>
   );
 }
---
+++ src/components/Table/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Table() {
+    return (
+        <div>
+            <table class="table table-striped">
+                ...
+            </table>
+        </div>
+    );
+}
+
+export default Table;
