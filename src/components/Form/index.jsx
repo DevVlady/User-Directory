5852562411270d4e08c7becca18d40ff3e4f0885
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-19 20:42:52 -0600

    Added the name and value to equal props.search inside the form.jsx file.

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -7,7 +7,7 @@ function Form(props) {
             <nav class="navbar navbar-light bg-light">
                 <div class="container-fluid-center">
                     <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" onChange={props.handleInputChange}/>
+                        <input name="search" class="form-control me-2" type="search" placeholder="Search" value={props.search} onChange={props.handleInputChange}/>
                     </form>
                 </div>
             </nav>
