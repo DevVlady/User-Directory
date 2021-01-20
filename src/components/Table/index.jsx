defd6fea35cfcd44f680ac1f31a13f6b05c6ec9e
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-19 21:22:35 -0600

    Using the api dot method i inputed the path to the proper data to correspond with the the heading of which all this data was being imported from and display on the page.

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -49,7 +49,7 @@ class Table extends React.Component {
     }
 
     render() {
-        const { classes } = this.props;
+        // const { classes } = this.props;
         return (
             <div>
                 <Search search={this.state.search} handleInputChange={this.handleInputChange} />
@@ -57,38 +57,40 @@ class Table extends React.Component {
                     <thead>
                         <tr>
                             <th scope="col">Image</th>
-                            <th scope="col" onClick={() => this.sortByName()}>Name</th>
-                            <th scope="col">Phone</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">DOB</th>
+                            <th onClick={() => this.sortByName()}>Name</th>
+                            <th>Phone</th>
+                            <th>Email</th>
+                            <th>DOB</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td></td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                            <td></td>
-                        </tr>
+                        {this.state.users
+                            .filter(
+                                (user) =>
+                                    user.name.first
+                                        .toLowerCase()
+                                        .includes(this.state.search.toLowerCase()) ||
+                                    user.name.last
+                                        .toLowerCase()
+                                        .includes(this.state.search.toLowerCase())
+                            )
+                            .map((user) => (
+                                <tr key={user.name}>
+                                    <td>
+                                        <img src={user.picture.thumbnail} alt=""></img>
+                                    </td>
+                                    <td>
+                                        {user.name.first} {user.name.last}
+                                    </td>
+                                    <td>{user.phone}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.dob.date}</td>
+                                </tr>
+                            ))}
                     </tbody>
                 </table>
             </div>
         );
-
     }
 }
 
