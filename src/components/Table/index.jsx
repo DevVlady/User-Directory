32947ebb6363dc11a9b1cfe8c7c0e712c3946a3e
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-19 20:51:05 -0600

    Added the class constructor function inside the table componenet.

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react';
+import './style.css';
+import EmployeeContext from '../../utils/employeeContext';
+
+class Table extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Image</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">DOB</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+
+    }
+}
+
+export default Table;
